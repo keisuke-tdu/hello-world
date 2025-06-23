ba59b78609cb9be9435e50fe07c87b8f2041d643
keisuke-tdu <tdu23fi062@gmail.com>
2025-06-23 14:55:07 +0900

    umakuike

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
-const express = require("express");
+const express = require('express')
+const expressWs = require('express-ws')
+
+const app = express()
+expressWs(app)
+
+const port = process.env.PORT || 3001
+let connects = []
+
+app.use(express.static('public'))
+
+app.ws('/ws', (ws, req) => {
+  connects.push(ws)
+
+  ws.on('message', (message) => {
+    console.log('Received:', message)
+
+    connects.forEach((socket) => {
+      if (socket.readyState === 1) {
+        // Check if the connection is open
+        socket.send(message)
+      }
+    })
+  })
+
+  ws.on('close', () => {
+    connects = connects.filter((conn) => conn !== ws)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
+
+/*const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -10,6 +44,7 @@ server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
 const html = `
+*/
 <!DOCTYPE html>
 <html>
   <head>
